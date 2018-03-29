0bf563a80f258fe2c33963d8b981032097e384a0
Kyle Treman <jkt290@nyu.edu>
2018-03-29 11:31:34 -0400

    Add meals table migration

---
+++ migrations/20180329111704_meals_table.js
@@ -0,0 +1,17 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('meals', (table) => {
+    table.increments();
+    table.string("Name");
+    table.string("Time");
+    table.enu('Tags', ['Spring', 'Summer', 'Fall', 'Winter', 'Chicken', 'Beef', 'Fish', 'Pork', 'Vegetarian']);
+    table.enu('Difficulty', ['Easy', 'Medium', 'Hard']);
+    table.text('Instructions');
+    table.string('img');
+    table.timestamps(true, true);
+  })
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('meals');
+};
