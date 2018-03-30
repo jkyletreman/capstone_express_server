bef284aaf5d72ad7927ec7530d9836a150b48b81
Kyle Treman <jkt290@nyu.edu>
2018-03-30 13:52:54 -0400

    Added second db entry

--- seeds/1test_meals.js
+++ seeds/1test_meals.js
@@ -5,7 +5,24 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex("meals").insert([
-        {id: 1, name: "Buffalo Chicken", time: "30 minutes", tags: "Summer", difficulty: "Easy", instructions: "Melt the butter. Once the butter is melted mix in the sauce. Preheat the oven to 400. Place chicken in a cast iron skillet and cover in sauce. Cook 20 minutes.", summary: 'Simple buffalo chicken in a castiron skillet.', img: "https://assets.bwbx.io/images/users/iqjWHBFdfxIU/i3Yp1mXpDIMA/v2/-1x-1.jpg"}
+        { id: 1,
+          name: "Buffalo Chicken",
+          time: "30 minutes",
+          tags: "Summer",
+          difficulty: "Easy",
+          instructions: "Melt the butter. Once the butter is melted mix in the sauce. Preheat the oven to 400. Place chicken in a cast iron skillet and cover in sauce. Cook 20 minutes.",
+          summary: 'Simple buffalo chicken in a castiron skillet.',
+          img: "https://assets.bwbx.io/images/users/iqjWHBFdfxIU/i3Yp1mXpDIMA/v2/-1x-1.jpg"
+        },
+        { id: 2,
+          name: "Jerk Chicken",
+          time: "30 minutes",
+          tags: "Summer",
+          difficulty: "Easy",
+          instructions: "Dry rub the chicken with jerk seasoning. Preheat the oven to 400. Place chicken on a baking sheet. Cook 20 minutes.",
+          summary: 'Simple jerk chicken in the oven.',
+          img: "https://cdn3.f-cdn.com/contestentries/1751/445921/29924-9381-13151323684e6353d06b7a7/rasta%20chicken%20by%20marattacks%20colored%20500_thumb900.jpg"
+        }
       ]);
     });
 };
--- seeds/2test_ingredients.js
+++ seeds/2test_ingredients.js
@@ -7,7 +7,9 @@ exports.seed = function(knex, Promise) {
       return knex("ingredients").insert([
         {id: 1, ingredient_id: 1, name: "Chicken", amount: 1, unit: "pound"},
         {id: 2, ingredient_id: 1, name: "Buffalo sauce", amount: 8, unit: "ounces"},
-        {id: 3, ingredient_id: 1, name: "Butter", amount: 4, unit: "tablespoons"}
+        {id: 3, ingredient_id: 1, name: "Butter", amount: 4, unit: "tablespoons"},
+        {id: 4, ingredient_id: 2, name: "Chicken", amount: 1, unit: "pound"},
+        {id: 5, ingredient_id: 2, name: "Seasoning", amount: 2, unit: "tablespoons"},
       ]);
     });
 };
