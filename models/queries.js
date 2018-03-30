4c148b9fcf0a6cd052db5f1bfe21dbeea6192996
Kyle Treman <jkt290@nyu.edu>
2018-03-30 10:48:08 -0400

    Add card query

--- models/queries.js
+++ models/queries.js
@@ -1,9 +1,16 @@
 const knex = require('../db');
 
 function testQuery() {
-  return knex('ingredients');
-}
+  return knex('meals')
+    .select('name', 'summary', 'img')
+};
+
+function getSmallCardInfo() {
+  return knex('meals')
+    .select('name', 'summary', 'img');
+};
 
 module.exports = {
-  testQuery
+  testQuery,
+  getSmallCardInfo
 };
--- server.js
+++ server.js
@@ -6,14 +6,23 @@ const app = express();
 const PORT = process.env.PORT || 5050;
 
 const {
-  testQuery
+  testQuery,
+  getSmallCardInfo
 } = require('./models/queries.js');
 
 app.use(morgan('tiny'));
 app.use(bodyParser.urlencoded({ extended: true}));
 app.use(bodyParser.json());
 
-app.get('/ingredients', (req, res) => {
+app.get('/smallCards', (req, res) => {
+  getSmallCardInfo(req).then(cards => {
+    res.format({
+      "application/json":() => res.json(cards)
+    })
+  })
+})
+
+app.get('/test', (req, res) => {
   testQuery(req).then(ingredients => {
     res.format({
       "application/json":() => res.json(ingredients)
