4cecf5ac5d2b9417e3893e25dd7f6a47f6059c24
Kyle Treman <jkt290@nyu.edu>
2018-04-17 12:02:31 -0400

    Add production config

--- knexfile.js
+++ knexfile.js
@@ -2,5 +2,9 @@ module.exports = {
   development: {
     client: 'pg',
     connection: 'postgres://localhost:5432/recipes'
+  },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL
   }
 }
